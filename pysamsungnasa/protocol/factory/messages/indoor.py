7885d4c11f6d5b4d9c35370165c34ee283ca8301
pantherale0 <pantherale0@users.noreply.github.com>
2026-01-02 11:16:56 +0000

    Add message parser: Modulating Valve 1 (0x42CA)

--- pysamsungnasa/protocol/factory/messages/indoor.py
+++ pysamsungnasa/protocol/factory/messages/indoor.py
@@ -1466,6 +1466,14 @@ class InVariableUnknown428dMessage(RawMessage):
     MESSAGE_NAME = "InVariableUnknown428dMessage"
 
 
+class InModulatingValve1Message(FloatMessage):
+    """Parser for message 0x42CA (Modulating Valve 1)."""
+
+    MESSAGE_ID = 0x42CA
+    MESSAGE_NAME = "Modulating Valve 1"
+    UNIT_OF_MEASUREMENT = "## Notes"
+
+
 class InFsv3046(FloatMessage):
     """Parser for message 0x42CE (FSV 3046 Disinfection Max Time)."""
 
